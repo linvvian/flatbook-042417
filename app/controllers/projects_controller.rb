7d16957e829ae3d46b7515c8a59dac2b62e06c5e
anneeb <ahuang852@gmail.com>
2017-05-30 20:55:37 -0400

    add views and controller methods and validations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,23 @@
 class ProjectsController < ApplicationController
+  before_action :logged_in
+  before_action :logged_member, only: [:edit, :update, :destroy]
   def index
     @cohorts = Cohort.all
   end
 
   def new
-
+    @project = Project.new
+    @other_students = current_user.cohort.users.where("id != #{current_user.id}")
   end
 
   def create
-    #code
+    project = Project.new(project_params(:name, :description, :github, :user_ids => []))
+    if project.save
+      project.users << current_user
+      redirect_to project_path(project)
+    else
+      redirect_to new_project_path
+    end
   end
 
   def show
@@ -17,12 +26,38 @@ class ProjectsController < ApplicationController
 
   def edit
     @project = Project.find(params[:id])
+    @other_students = current_user.cohort.users.where("id != #{current_user.id}")
   end
 
   def update
+    project = Project.find(params[:id])
+    project.update(project_params(:name, :description, :github, :user_ids => []))
+    if project.save
+      project.users << current_user
+      redirect_to project_path(project)
+    else
+      redirect_to edit_project_path(project)
+    end
   end
 
   def destroy
-    #code
+    project = Project.find(params[:id])
+    project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params(*args)
+    params.require(:project).permit(*args)
+  end
+
+  def project_member
+    project = Project.find(params[:id])
+    project.users.include?(current_user)
+  end
+
+  def logged_member
+    redirect_to projects_path unless project_member
   end
 end
