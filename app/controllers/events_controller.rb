b74f46f174a7c14647315e0adf414a351114e0c0
anneeb <ahuang852@gmail.com>
2017-06-01 10:33:39 -0400

    added permissions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :logged_in
   before_action :find_event, except: [:index, :new, :create]
+  before_action :logged_creator, only: [:edit, :update, :destroy]
   helper_method :is_member?, :is_creator?
 
   def index
@@ -58,4 +59,9 @@ class EventsController < ApplicationController
   def is_creator?
     @event.creator == current_user
   end
+
+  def logged_creator
+    redirect_to event_path(@event) unless is_creator? || is_admin?
+  end
+
 end
