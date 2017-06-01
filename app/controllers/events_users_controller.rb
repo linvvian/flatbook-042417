ecad59aac54721a6ab818a237488a1e968c4f5b9
linvvian <lin.vvian@gmail.com>
2017-06-01 10:34:41 -0400

    removes edit delete buttons based on permissions

--- app/controllers/events_users_controller.rb
+++ app/controllers/events_users_controller.rb
@@ -1,11 +1,13 @@
 class EventsUsersController < GroupsController
 
   def join_event
+    @event = Event.find(params[:id])
     @event.users << current_user
     redirect_to event_path(@event)
   end
 
   def leave_event
+    @event = Event.find(params[:id])
     @event.users.delete(current_user)
     redirect_to event_path(@event)
   end
