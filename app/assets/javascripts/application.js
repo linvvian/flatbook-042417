786a480383db18bdd02b6cf69816547cf56c9d21
Jack H <jackhe678@gmail.com>
2017-06-02 14:39:43 -0400

    set google map to flatiron

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@
 
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 40.705247, lng: -74.014034};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: uluru
--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,8 +1,8 @@
 
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 40.705247, lng: -74.014034};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 17,
     center: uluru
   });
   var marker = new google.maps.Marker({
