b0c7fcdde7827df459e93365f02afe23f6a17290
linvvian <lin.vvian@gmail.com>
2017-06-02 11:06:05 -0400

    fixes delete user with thumbs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects
   has_secure_password
   validates :first_name, :last_name, presence: true
-  validates :email, uniqueness: true
   # validate :flatiron_email
 
   def name
