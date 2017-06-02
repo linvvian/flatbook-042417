be28c51c69807cbaf780b93b2a24e3f9ad3c6e5d
anneeb <ahuang852@gmail.com>
2017-06-02 13:33:48 -0400

    remove byebug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   before_action :logged_author, only: :delete_comments
 
   def create
-    byebug
     comment = Comment.new(comment_params)
     comment.send("#{params[:comment][:page_type].downcase}_id=", params[:comment][:page_id])
     if comment.save
