a2753cfe3289c52e39c6f0dc7d5703552de68adc
Jack H <jackhe678@gmail.com>
2017-06-02 13:24:24 -0400

    fixed modle in common to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170602132826) do
-=======
-ActiveRecord::Schema.define(version: 20170602130711) do
->>>>>>> activity
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +77,6 @@ ActiveRecord::Schema.define(version: 20170602130711) do
     t.integer  "friend_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "confirm",    default: false
     t.string   "status",     default: "not"
   end
 
